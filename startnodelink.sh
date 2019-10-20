1781939d02dc2c1dc3c1c053db217d4ce7f58c1b
scyto <lx_b@msn.com>
2019-10-20 03:05:17 -0700

    update for dot net core version

--- startnodelink.sh
+++ startnodelink.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
 ECHO checking if NodeLink exists
-if [ ! -e "/NodeLink/NodeLink.exe" ]; then
-wget -O /NodeLink/NodeLink.exe http://automationshack.com/Files/NodeLink.exe
+if [ ! -e "/NodeLink/NodeLink.dll" ]; then
+wget -O /NodeLink/NodeLink.dll http://automationshack.com/Files/NodeLink.dll 
 fi
+
+if [ ! -e "NodeLink.runtimeconfig.json" ]; then
+wget -O /NodeLink/NodeLink.runtimeconfig.json http://automationshack.com/Files/NodeLink.runtimeconfig.json
+fi
+
+
 ECHO starting NodeLink
-mono /NodeLink/NodeLink.exe &
+dotnet /NodeLink/NodeLink.dll &
 /bin/bash
